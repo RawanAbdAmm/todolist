b4a901586eca090dff059c60f7d2a13498c80cf7
RawanAbdAmm <rawanabdamm@gmail.com>
2024-01-20 19:38:30 +0300

    oout

--- lib/widgets/bodywidgets/notes_body_view.dart
+++ lib/widgets/bodywidgets/notes_body_view.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/read_note/read_notes_cubit.dart';
+import 'package:notes_app/screens/login.dart';
 import 'package:notes_app/widgets/bodywidgets/notes_listview.dart';
 import 'package:notes_app/widgets/customwidgets/custom_appbar.dart';
 
@@ -19,20 +20,13 @@ class _NotesViewBodyState extends State<NotesViewBody> {
   void initState() {
     BlocProvider.of<ReadNotesCubit>(context).readAllNotes();
     super.initState();
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user == null) {
-        // User signed out
-        print("User signed out");
-      } else {
-        // User signed in
-        print("User signed in: ${user.uid}");
-      }
-    });
   }
 
   Future<void> _signOut() async {
     try {
       await FirebaseAuth.instance.signOut();
+      Navigator.of(context)
+          .pushNamedAndRemoveUntil(LoginPage.id, (route) => false);
     } catch (e) {
       print("Error signing out: $e");
     }
@@ -44,7 +38,7 @@ class _NotesViewBodyState extends State<NotesViewBody> {
       padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
       child: Column(
         children: [
-         const SizedBox(
+          const SizedBox(
             height: 20,
           ),
           CustomAppbar(
@@ -53,7 +47,7 @@ class _NotesViewBodyState extends State<NotesViewBody> {
             icon: const Icon(Icons.exit_to_app), // Sign-out icon
             onpressed: _signOut,
           ),
-         const SizedBox(
+          const SizedBox(
             height: 30,
           ),
           Expanded(child: NotesListview()),
